53f04831e68980931a9c3553114108d2ce9aa2fb
BaconCatBug <BaconCatBug@users.noreply.github.com>
2019-03-20 22:34:13 +0000

    3.08

--- Export Inventory (EDIT ME TO HAVE THE CORRECT IP).bat
+++ Export Inventory (EDIT ME TO HAVE THE CORRECT IP).bat
@@ -1,2 +1,2 @@
 title FFRK Inventory Exporter
-mitmdump -q --listen-host xxx.xxx.xxx.xxx -s ffrk_inventory_tracker_v3.07.py 
+mitmdump -q --listen-host xxx.xxx.xxx.xxx -s ffrk_inventory_tracker_v3.08.py 
--- Track Drops (EDIT ME TO HAVE THE CORRECT IP).bat
+++ Track Drops (EDIT ME TO HAVE THE CORRECT IP).bat
@@ -1,2 +1,2 @@
 title FFRK Drop Tracker
-mitmdump -q --listen-host xxx.xxx.xxx.xxx -s ffrk_drop_tracker_v3.07.py
+mitmdump -q --listen-host xxx.xxx.xxx.xxx -s ffrk_drop_tracker_v3.08.py
