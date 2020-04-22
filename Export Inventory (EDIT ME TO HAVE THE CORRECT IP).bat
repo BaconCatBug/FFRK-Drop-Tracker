4c5a670797237d724ac1fa0daf19977952f1d9b2
BaconCatBug <BaconCatBug@gmail.com>
2020-04-22 21:47:13 +0100

    -

--- Export Inventory (EDIT ME TO HAVE THE CORRECT IP).bat
+++ Export Inventory (EDIT ME TO HAVE THE CORRECT IP).bat
@@ -1,2 +1,2 @@
 title FFRK Inventory Exporter
-mitmdump -q --listen-host xxx.xxx.xxx.xxx --listen-port 8888 -s FFRK_Inventory_Exporter_v6.07.py
+mitmdump -q --listen-host xxx.xxx.xxx.xxx --listen-port 8888 -s FFRK_Inventory_Exporter_v6.09.py
